7c2ea456bbb3804a21feb5c9b9a9e7ef8c3863b7
DavidSouzaxz <daviddavi1221@gmail.com>
2026-06-07 10:58:24 -0300

    Fix: ajusted auth requisition
    Signed-off-by: DavidSouzaxz <daviddavi1221@gmail.com>

--- src/main/java/com/projetoiot/plantainteligente/controller/AuthController.java
+++ src/main/java/com/projetoiot/plantainteligente/controller/AuthController.java
@@ -14,9 +14,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500", "*"})
 @Tag(name = "Autenticação", description = "Endpoints para cadastro e login de usuários")
 public class AuthController {
 
